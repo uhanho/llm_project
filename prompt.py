7dc8551451acd5e529350ddb61220e47a6fbdd04
TonyWild <sungu0027@naver.com>
2024-07-06 15:07:24 +0900

    integrated with DB

--- prompt.py
+++ prompt.py
@@ -5,17 +5,21 @@ from langchain_core.runnables import RunnablePassthrough
 from langchain_community.vectorstores.oraclevs import OracleVS
 from dotenv import load_dotenv
 import ast
+from collections import defaultdict
+import database
 
 load_dotenv()
-def prompt_create(city, date, date2, number, prompt):
-    database = OracleVS.Database()
-    retriever = database.get_retriever()
+def prompt_create(city, date, date2, number, question):
+    db = database.Database()
+    db.set_vector_store()
+    retriever = db.get_retriever()
+
     llm = ChatUpstage()
 
     template = """Create an hourly travel plan based on the following information and prompt.
         travel destination: {city}, first day: {date}, last day: {date2}, number of people: {number} 
-        Prompt: {prompt}
-        Recommend the places based on these: {context}
+        Prompt: {question}
+        Recommend the places based on these if related: {context}
 
         The output should be a list of 3-tuples: [(X, Y, Z), (X1, Y1, Z1), ...].
         The first element is a date with the summary of its day. (for example, XXXX-XX-XX (Day 1: 출발 및 XX 도착))
@@ -31,9 +35,8 @@ def prompt_create(city, date, date2, number, prompt):
     # date 형식에 따라 입력 형식 바꿀 수 있음
 
     while(True):
-        chain = {"context": retriever, "prompt": RunnablePassthrough(), "city": city, "date": date, "date2": date2, "number": number} | prompt_template | llm | StrOutputParser() | llm | StrOutputParser()
-        response = chain.invoke(prompt)
-        print(response)
+        chain = prompt_template | llm | StrOutputParser()
+        response = chain.invoke({"context": retriever, "question": question, "city": city, "date": date, "date2": date2, "number": number})
         try:
             if not isinstance(ast.literal_eval(response), list):
                 continue
@@ -41,6 +44,15 @@ def prompt_create(city, date, date2, number, prompt):
         except:
             continue
 
-    return response
+    tbl = ast.literal_eval(response)
+    result = defaultdict(list)
+    for t in tbl:
+        key = t[0]
+        value = (t[1], t[2])
+        result[key].append(value)
 
-prompt_create("도쿄", "2024-07-06", "2024-07-09", 1, "여유로운 여행을 원해")
+    transformed_list = [(key, value) for key, value in result.items()]
+
+    return transformed_list
+
+# prompt_create("부산", "2024-07-06", "2024-07-09", 1, "여유로운 여행을 원해")
